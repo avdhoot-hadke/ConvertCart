7e9afdbff5586f0fb675b5ac3a8e787e56c92478
Avdhoot Hadke <avdhoothadke@gmail.com>
2025-08-08 16:07:02 +0530

    modified product structure for vercel

--- services/product-service/api/index.js
+++ services/product-service/api/index.js
@@ -1,12 +1,11 @@
-// index.js
 import express, { json } from "express";
 import { connect } from "mongoose";
 import cors from "cors";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import dotenv from "dotenv";
-import ingestProducts from "./controllers/ingestController.js";
-import getProducts from "./controllers/productController.js";
+import ingestProducts from "../src/controllers/ingestController.js";
+import getProducts from "../src/controllers/productController.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,17 +14,14 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-dotenv.config({ path: resolve(__dirname, "../.env") });
+dotenv.config({ path: resolve(__dirname, "../../.env") });
 
 connect(process.env.DB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.post("/ingest", ingestProducts);
-
 app.get("/products", getProducts);
-
 app.get("/", (req, res) => res.send("API running."));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
